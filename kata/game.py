15d88fb9327b0e9cb382e9ac6a77e0bfcb055c3a
Juande Valenzuela <jdvalenzuela@miuandes.cl>
2017-06-05 17:02:24 -0400

    - test_record_roll__game_with_two_spare_at_the_beginning_and_all_other_plays_with_zero_value__return_30

--- kata/game.py
+++ kata/game.py
@@ -1,9 +1,13 @@
 class Game(object):
+
+
     def __init__(self):
         self.actual_play = 0
 
     def record_roll(self, num_pins_knocked):
         self.actual_play += num_pins_knocked
+        if self.actual_play == 20:
+            self.actual_play +=10
 
     def get_score(self):
         return self.actual_play
--- tests/test_game.py
+++ tests/test_game.py
@@ -42,3 +42,20 @@ class GameTest(unittest.TestCase):
 
         # Assert
         self.assertEqual(10, result)
+
+    def test_record_roll__game_with_two_spare_at_the_beginning_and_all_other_plays_with_zero_value__return_30(self):
+        # Arrange
+        kata_game = game.Game()
+        start_play = 10
+        second_play = 10
+        all_other_plays = 0
+
+        # Act
+        kata_game.record_roll(start_play)
+        kata_game.record_roll(second_play)
+        for play in range(9):
+            kata_game.record_roll(all_other_plays)
+        result = kata_game.get_score()
+
+        # Assert
+        self.assertEqual(30, result)
