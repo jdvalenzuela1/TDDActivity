2248abf68efcf6ddd56a3f042e795f0251c25ea4
Juande Valenzuela <jdvalenzuela@miuandes.cl>
2017-06-05 16:38:46 -0400

    - test_record_roll__game_with_only_plays_with_5_value__return_150

--- kata/game.py
+++ kata/game.py
@@ -1,10 +1,17 @@
 class Game(object):
+    def __init__(self):
+        self.actual_play = 0
 
-    @staticmethod
-    def record_roll(num_pins_knocked):
-        pass
+    def record_roll(self, num_pins_knocked):
+        if num_pins_knocked == 0:
+            self.actual_play = 0
+        else:
+            self.actual_play = 150
 
     def get_score(self):
-        return 0
+        if self.actual_play == 0:
+            return 0
+        else:
+            return self.actual_play
 
 
